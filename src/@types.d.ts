1b98e4a90368292bb7a266c2626f199dfb9d50ea
duhanmeric <duhanmeric@gmail.com>
2024-04-07 19:55:20 +0300

    Delete unused SVG files and remove unused code

--- src/@types.d.ts
+++ src/@types.d.ts
@@ -1,14 +1,3 @@
-type ActionReturn<T> =
-    | {
-          success: true;
-          data: T;
-      }
-    | {
-          success: false;
-          message: string;
-          data: null;
-      };
-
 type SearchParams = { [key: string]: string };
 
 type Params = {
